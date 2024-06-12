3388cb81d7aecc643870a09f88eabae7b93e5681
LiliBalan <t-lilbalan@itesm.mx>
2024-06-12 19:28:45 +0000

    agregue cambios para azure reto 4

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,17 @@
 // Placeholder to host a component
+const Hours = () => {
+    const hours = Array.from({ length: 24 }, (_, i) => i);
+   
+    return (
+  <div>
+  <h2>Horas del día</h2>
+  <ul>
+          {hours.map((hour) => (
+  <li key={hour}>{hour}:00</li>
+          ))}
+  </ul>
+  </div>
+    );
+  };
+   
+  export default Hours;
